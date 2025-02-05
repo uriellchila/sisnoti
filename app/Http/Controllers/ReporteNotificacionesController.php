5ce74b7f877e9e676a25b1e08c027bf3ad142f5a
uriellchila <uriel.chila@gmail.com>
2025-02-05 09:28:21 -0500

    7ta Actualizacion

--- app/Http/Controllers/ReporteNotificacionesController.php
+++ app/Http/Controllers/ReporteNotificacionesController.php
@@ -316,7 +316,7 @@ class ReporteNotificacionesController extends Controller
         DB::raw('count(distinct numero_doc) as cantidad'), 
         DB::raw('count(distinct fecha_notificacion) as dias'), 
        //B::raw('count(*)/count(distinct fecha_notificacion)::DOUBLE PRECISION as promedio')
-       B::raw('count(*)/count(distinct fecha_notificacion) as promedio')
+       DB::raw('count(*)/count(distinct fecha_notificacion) as promedio')
       /* DB::raw(
             '(select count(*) from notificacion_documentos as nd
             where nd.deleted_at is null and u.id = nd.user_id) as dias'),*/
